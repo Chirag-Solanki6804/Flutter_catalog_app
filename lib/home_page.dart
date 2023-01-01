56e60851972ce98a08808d1b510167a9612d3374
chirag_Solanki <sc494802@gmail.com>
2023-01-01 12:00:14 +0530

    day2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final String name = "chirag Solanki";
+    final int age = 19;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Hello Pro $name . You are $age year old."),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
